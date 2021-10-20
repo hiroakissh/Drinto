7876efa2027a85c7b3c443432d2e5899a3b2bbe7
hirokaissh <hirokai.saito1014@gmail.com>
2021-10-20 13:22:26 +0900

    StoryBoradの作成

---
+++ Drinto/Controller/FirestViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FirestViewController.swift
+//  Drinto
+//
+//  Created by HiroakiSaito on 2021/10/20.
+//
+
+import UIKit
+
+class FirestViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
