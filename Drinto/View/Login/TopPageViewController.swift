61044fa32a4014ea619d1cfedd0e86aa92348551
hirokaissh <hirokai.saito1014@gmail.com>
2022-05-20 17:03:48 +0900

    [update] 立ち上げ直後の画面を作成

--- Drinto/View/Login/TopPageViewController.swift
+++ Drinto/View/Login/TopPageViewController.swift
@@ -9,21 +9,18 @@ import UIKit
 
 class TopPageViewController: UIViewController {
 
+    @IBOutlet private weak var SignInButton: UIButton!
+    @IBOutlet private weak var SignUpButton: UIButton!
+    @IBOutlet private weak var getSignInButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        settingUI()
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func settingUI() {
+        SignInButton.layer.cornerRadius = 15.0
+        SignUpButton.layer.cornerRadius = 15.0
     }
-    */
-
 }
