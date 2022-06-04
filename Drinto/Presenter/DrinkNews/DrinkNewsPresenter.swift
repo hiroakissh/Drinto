dd03b8ad65886fc979b20d8437edf4ebfb291805
hirokaissh <hirokai.saito1014@gmail.com>
2022-06-04 09:26:31 +0900

    [add] NewsDataのPresenterを作成

---
+++ Drinto/Presenter/DrinkNews/DrinkNewsPresenter.swift
@@ -0,0 +1,20 @@
+//
+//  DrinkNewsProtocol.swift
+//  Drinto
+//
+//  Created by HiroakiSaito on 2022/06/04.
+//
+
+import Foundation
+
+protocol DrinkNewsPresenterInput {
+
+}
+
+protocol DrinkNewsPresenterOutput {
+
+}
+
+class DrinkNewsPresenter: DrinkNewsPresenterInput {
+
+}
