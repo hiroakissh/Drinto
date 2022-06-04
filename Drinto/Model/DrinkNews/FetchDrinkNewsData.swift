c0b45de1365d2e69cc2b78e45eaab284df6da4a9
hirokaissh <hirokai.saito1014@gmail.com>
2022-06-04 09:25:30 +0900

    [add] presenterロジックに適合した関数の作成

--- Drinto/Model/DrinkNews/FetchDrinkNewsData.swift
+++ Drinto/Model/DrinkNews/FetchDrinkNewsData.swift
@@ -7,14 +7,18 @@
 
 import Foundation
 
-class FetchDrinkNewsData {
+protocol NewsAPIDataModelInput {
+    func fetchDrinkNewsData(completion: @escaping((Result<[NewsData], Error>) -> ()))
+}
+
+class FetchDrinkNewsData: NewsAPIDataModelInput {
 
     let apiKey = "84b4753e4e64481280f134d01ddfe9af"
     let urlHead = "https://newsapi.org/v2/everything?"
     let sort = "popularity"
     var category = "tea"
 
-    func getNewsFromNewsAPI(completionHandler: @escaping(NewsData) -> Void) {
+    func getNewsFromNewsAPI(completion: @escaping(NewsData) -> Void) {
         let newsURLString = "\(urlHead)q=\(category)&apiKey=\(apiKey)"
         print(newsURLString)
         guard let url = URL(string: newsURLString) else { return }
@@ -30,7 +34,7 @@ class FetchDrinkNewsData {
                 let jsonDecoder = JSONDecoder()
                 do {
                     let jsonDecoderData = try jsonDecoder.decode(NewsData.self, from: data)
-                    completionHandler(jsonDecoderData)
+                    completion(jsonDecoderData)
                 } catch {
                     print("Decode Error")
                     print(error.localizedDescription)
@@ -38,5 +42,25 @@ class FetchDrinkNewsData {
             }
         }
         task.resume()
+    }
+
+    func fetchDrinkNewsData(completion: @escaping ((Result<NewsData, Error>) -> ())) {
+        let newsURLString = "\(urlHead)q=\(category)&apiKey=\(apiKey)"
+        print(newsURLString)
+        guard let url = URL(string: newsURLString) else { return }
+        let session = URLSession(configuration: .default)
+        let task = session.dataTask(with: url) { data, _, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            let jsonDecoder = JSONDecoder()
+            guard let data = data, let decodedData = try? jsonDecoder.decode(NewsData.self, from: data) else {
+                let error = NSError(domain: "parse-error", code: 1, userInfo: nil)
+                completion(.failure(error))
+                return
+            }
+            completion(.success(decodedData))
+        }
     }
 }
