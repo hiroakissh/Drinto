abe1f1b0e54cbd79644a9dce2e6dcccc7fcbaaad
hirokaissh <hirokai.saito1014@gmail.com>
2022-06-06 22:52:29 +0900

    [add]画面遷移の画面の追加，ViewControllerの追加，戻る機能の追加

---
+++ Drinto/View/DrinkNews/DrinkNewsDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DrinkNewsDetailViewController.swift
+//  Drinto
+//
+//  Created by HiroakiSaito on 2022/06/06.
+//
+
+import UIKit
+import WebKit
+
+class DrinkNewsDetailViewController: UIViewController {
+    @IBOutlet private weak var webView: WKWebView!
+    @IBOutlet private weak var navigationTitle: UINavigationItem!
+
+    var newsData: Article?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        guard let newsData = newsData else {
+            return
+        }
+        navigationTitle.title = newsData.title
+        guard let newsURL = URL(string: newsData.url) else {
+            return
+        }
+        let newsRequest = URLRequest(url: newsURL)
+        webView.load(newsRequest)
+        webView.allowsBackForwardNavigationGestures = true
+    }
+    @IBAction func backButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
