18cc29f300ac1921e650ac5536500a82e77706df
hiroakissh <hiroaki.saito1014@gmail.com>
2022-07-03 11:24:00 +0900

    [add] pointを記載する箇所をPickerViewで値を記入

--- Drinto/View/AddDrink/AddDrinkViewController.swift
+++ Drinto/View/AddDrink/AddDrinkViewController.swift
@@ -18,11 +18,23 @@ class AddDrinkViewController: UIViewController {
     @IBOutlet private weak var drinkNameTextField: UITextField!
     @IBOutlet private weak var categoryTextField: UITextField!
 
+    private var pickerView = UIPickerView()
+    private var editTextFiled: UITextField?
+
+    private let point: [String] = ["0", "1", "2", "3", "4", "5"]
+
     // swiftlint:disable force_try
     private let realm = try! Realm()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        createPickerView()
+        value1TextField.delegate = self
+        value2TextField.delegate = self
+        value3TextField.delegate = self
+        value4TextField.delegate = self
+        value5TextField.delegate = self
+        value6TextField.delegate = self
     }
     @IBAction func addButtonAction(_ sender: Any) {
         let realmModel = DrinkMemoryRealmModel()
@@ -82,6 +94,86 @@ class AddDrinkViewController: UIViewController {
         catch {
             print(error)
         }
+    }
+}
 
+extension AddDrinkViewController {
+    @objc func donePicker() {
+        value1TextField.endEditing(true)
+        value2TextField.endEditing(true)
+        value3TextField.endEditing(true)
+        value4TextField.endEditing(true)
+        value5TextField.endEditing(true)
+        value6TextField.endEditing(true)
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        value1TextField.endEditing(true)
+        value2TextField.endEditing(true)
+        value3TextField.endEditing(true)
+        value4TextField.endEditing(true)
+        value5TextField.endEditing(true)
+        value6TextField.endEditing(true)
+    }
+}
+
+extension AddDrinkViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        point.count
+    }
+
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        point[row]
+    }
+
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if editTextFiled == value1TextField {
+            value1TextField.text = point[row]
+        } else if editTextFiled == value2TextField {
+            value2TextField.text = point[row]
+        } else if editTextFiled == value3TextField {
+            value3TextField.text = point[row]
+        } else if editTextFiled == value4TextField {
+            value4TextField.text = point[row]
+        } else if editTextFiled == value5TextField {
+            value5TextField.text = point[row]
+        } else if editTextFiled == value6TextField {
+            value6TextField.text = point[row]
+        }
+    }
+
+    func createPickerView() {
+        pickerView.delegate = self
+        value1TextField.inputView = pickerView
+        value2TextField.inputView = pickerView
+        value3TextField.inputView = pickerView
+        value4TextField.inputView = pickerView
+        value5TextField.inputView = pickerView
+        value6TextField.inputView = pickerView
+
+        let toolbar = UIToolbar()
+        toolbar.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: 44)
+        let doneButtonItem = UIBarButtonItem(
+            barButtonSystemItem: .done,
+            target: self,
+            action: #selector(AddDrinkViewController.donePicker)
+        )
+        toolbar.setItems([doneButtonItem], animated: true)
+        value1TextField.inputAccessoryView = toolbar
+        value2TextField.inputAccessoryView = toolbar
+        value3TextField.inputAccessoryView = toolbar
+        value4TextField.inputAccessoryView = toolbar
+        value5TextField.inputAccessoryView = toolbar
+        value6TextField.inputAccessoryView = toolbar
+    }
+}
+
+extension AddDrinkViewController: UITextFieldDelegate {
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        editTextFiled = textField
+        return true
     }
 }
