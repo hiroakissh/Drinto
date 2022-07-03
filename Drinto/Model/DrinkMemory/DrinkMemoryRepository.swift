6c56f8e3f573e8bfe1857a45418d9a27b57c7ea2
hiroakissh <hiroaki.saito1014@gmail.com>
2022-07-03 15:58:41 +0900

    [update] Realm swiftに追加する機能の追加

--- Drinto/Model/DrinkMemory/DrinkMemoryRepository.swift
+++ Drinto/Model/DrinkMemory/DrinkMemoryRepository.swift
@@ -8,26 +8,56 @@
 import Foundation
 import RealmSwift
 
-//protocol DrinkMemorySwiftModelInput {
+protocol DrinkMemorySwiftModelInput {
 //    func fetchDrinkMemoryData(completion: @escaping((Result<[DrinkMemorySwiftModel], Error>) -> Void))
-//    func addDrinkMemoryData(_ drinkMemoryData: DrinkMemorySwiftModel) -> [DrinkMemorySwiftModel]
-//    func removeDrinkMemoryData(at index: Int)
-//    func updateDrinkMemoryData(at index: Int)
-//}
-//
-//class DrinkMemoryRepository: DrinkMemorySwiftModelInput {
-//    // swiftlint:disable:next force_try
-//    private let realm = try! Realm()
-//
+    func addDrinkMemoryData(_ drinkMemoryData: DrinkMemorySwiftModel)
+    func removeDrinkMemoryData(at index: Int)
+    func updateDrinkMemoryData(at index: Int)
+}
+
+class DrinkMemoryRepository: DrinkMemorySwiftModelInput {
+    // swiftlint:disable:next force_try
+    private let realm = try! Realm()
+
 //    func fetchDrinkMemoryData(completion: @escaping ((Result<[DrinkMemorySwiftModel], Error>) -> Void)) {
 //
 //    }
-//
-//    func addDrinkMemoryData(_ drinkMemoryData: DrinkMemorySwiftModel) {
-//        let
-//    }
-//
-//    func removeDrinkMemoryData(at index: Int) {
-//
-//    }
-//}
+
+    func addDrinkMemoryData(_ drinkMemoryData: DrinkMemorySwiftModel) {
+        let realmModel = DrinkMemoryRealmModel()
+        let realmDrinkPoint = DrinkPoint()
+        let uuid = UUID()
+
+        realmModel.drinkMemoryUUID = drinkMemoryData.uuidString
+        realmModel.drinkName = drinkMemoryData.drinkName
+        realmModel.category = drinkMemoryData.category
+        realmModel.imagePath = drinkMemoryData.imagePath
+
+        realmDrinkPoint.value1 = drinkMemoryData.drinkPoint?[0] ?? 0
+        realmDrinkPoint.value2 = drinkMemoryData.drinkPoint?[1] ?? 0
+        realmDrinkPoint.value3 = drinkMemoryData.drinkPoint?[2] ?? 0
+        realmDrinkPoint.value4 = drinkMemoryData.drinkPoint?[3] ?? 0
+        realmDrinkPoint.value5 = drinkMemoryData.drinkPoint?[4] ?? 0
+        realmDrinkPoint.value6 = drinkMemoryData.drinkPoint?[5] ?? 0
+
+        realmModel.drinkPoint.append(realmDrinkPoint)
+
+        do {
+            try realm.write {
+                realm.add(realmModel)
+            }
+        } catch {
+            print("Realm Add Error")
+            return
+        }
+        print(realmModel)
+    }
+
+    func removeDrinkMemoryData(at index: Int) {
+        print(index)
+    }
+
+    func updateDrinkMemoryData(at index: Int) {
+        print(index)
+    }
+}
