829f45806a57a0dee7917208b157007848863a7e
hiroakissh <hiroaki.saito1014@gmail.com>
2022-07-13 22:54:08 +0900

    [add] ViewControllerの追加

---
+++ Drinto/View/DrinkMemory/DetailDrinkMemoryViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailDrinkMemoryViewController.swift
+//  Drinto
+//
+//  Created by HiroakiSaito on 2022/07/13.
+//
+
+import UIKit
+
+class DetailDrinkMemoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
