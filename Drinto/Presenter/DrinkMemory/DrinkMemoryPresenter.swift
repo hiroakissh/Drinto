31ca0e61275ef1b1082e9942ede0087fb2a388f7
hiroakissh <hiroaki.saito1014@gmail.com>
2022-07-22 13:51:41 +0900

    [add] Presenterロジックの作成

--- Drinto/Presenter/DrinkMemory/DrinkMemoryPresenter.swift
+++ Drinto/Presenter/DrinkMemory/DrinkMemoryPresenter.swift
@@ -10,6 +10,7 @@ import Foundation
 protocol DrinkMemoryPresenterInput {
     var numberOfDrinkMemory: Int { get }
     func drinkMemory(forRow row: Int) -> DrinkMemorySwiftModel?
+    func viewDidLoad()
     func didSelectRowAt(_ indexPath: IndexPath)
 }
 
@@ -17,6 +18,31 @@ protocol DrinkMemoryPresenterOutput {
     func didFetch(_ drinkMemory: [DrinkMemorySwiftModel])
 }
 
-class DrinkMemoryPresenter {
+class DrinkMemoryPresenter: DrinkMemoryPresenterInput {
+    var drinkMemory = [DrinkMemorySwiftModel]()
 
+    var view: DrinkMemoryPresenterOutput?
+    var model: DrinkMemoryRepository
+
+    init (with view: DrinkMemoryPresenterOutput) {
+        self.view = view
+        self.model = DrinkMemoryRepository()
+    }
+
+    var numberOfDrinkMemory: Int {
+        drinkMemory.count
+    }
+
+    func drinkMemory(forRow row: Int) -> DrinkMemorySwiftModel? {
+        let drinkMemory = model.readDrinkMemoryData()
+        let indexDrinkMemory = drinkMemory[row]
+        return indexDrinkMemory
+    }
+
+    func didSelectRowAt(_ indexPath: IndexPath) {
+    }
+
+    func viewDidLoad() {
+        drinkMemory = model.readDrinkMemoryData()
+    }
 }
