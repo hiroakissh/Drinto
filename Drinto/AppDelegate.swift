e7593782e965d9b2952a237e4c8566b3022f8a96
hiroakissh <hiroaki.saito1014@gmail.com>
2022-12-24 13:02:54 +0900

    [add] 初期化コードの追加

--- Drinto/AppDelegate.swift
+++ Drinto/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import RealmSwift
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         print(Realm.Configuration.defaultConfiguration.fileURL!)
 //        //swiftlint:disable force_try
 //        try! Realm.deleteFiles(for: Realm.Configuration.defaultConfiguration)
+        FirebaseApp.configure()
         return true
     }
 
