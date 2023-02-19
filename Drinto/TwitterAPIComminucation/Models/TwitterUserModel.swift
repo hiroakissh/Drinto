a156d29e6122ffc6ddacf1f35546d566b6c20f10
hiroakissh <hiroaki.saito1014@gmail.com>
2023-02-20 08:57:27 +0900

    [update] TwitterUserのstructに更新

---
+++ Drinto/TwitterAPIComminucation/Models/TwitterUserModel.swift
@@ -0,0 +1,15 @@
+//
+//  TwitterUserModel.swift
+//  Drinto
+//
+//  Created by HiroakiSaito on 2023/02/20.
+//
+
+import Foundation
+
+struct TwitterUserModel {
+    let id: String
+    let userId: String
+    let userName: String
+    let profileImageURL: String
+}
