eefa6cf15841bcf30d948d9b50421ecfd2d795bd
hiroakissh <hiroaki.saito1014@gmail.com>
2023-02-20 10:31:44 +0900

    [update] ダミーデータの表示確認

--- Drinto/TwitterAPIComminucation/Views/TwitterAPIResultViewController.swift
+++ Drinto/TwitterAPIComminucation/Views/TwitterAPIResultViewController.swift
@@ -8,9 +8,46 @@
 import UIKit
 
 class TwitterAPIResultViewController: UIViewController {
+    @IBOutlet private weak var tweetResultTableView: UITableView!
+
+    private var tweets: [TweetModel] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        tweetResultTableView.dataSource = self
+        tweetResultTableView.delegate = self
 
+        // ダミー
+        let testUser = TwitterUserModel(
+            id: "1234",
+            userId: "5678",
+            userName: "testUser",
+            profileImageURL: "https://test"
+        )
+
+        let testTweet = TweetModel(id: "91011", text: "テスト", user: testUser)
+
+        tweets.append(testTweet)
+        tweetResultTableView.reloadData()
+    }
+}
+
+extension TwitterAPIResultViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("タップ")
+    }
+}
+
+extension TwitterAPIResultViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tweets.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let tweetCell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath)
+        var content = tweetCell.defaultContentConfiguration()
+        content.text = tweets[0].text
+        tweetCell.contentConfiguration = content
+        return tweetCell
     }
 }
