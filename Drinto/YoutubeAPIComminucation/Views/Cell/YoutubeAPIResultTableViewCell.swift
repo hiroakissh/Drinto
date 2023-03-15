a036a73f13f7de0d2337b28b96d618121df237a0
hiroakissh <hiroaki.saito1014@gmail.com>
2023-03-15 20:31:28 +0900

    [update] UIの変更

--- Drinto/YoutubeAPIComminucation/Views/Cell/YoutubeAPIResultTableViewCell.swift
+++ Drinto/YoutubeAPIComminucation/Views/Cell/YoutubeAPIResultTableViewCell.swift
@@ -16,6 +16,7 @@ class YoutubeAPIResultTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        thumbnailImageView.layer.cornerRadius = 10.0
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
