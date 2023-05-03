173bc1b83138034811f259fd8eef98231bd327c2
hiroakissh <hiroaki.saito1014@gmail.com>
2023-05-04 00:10:59 +0900

    [add] リファクタリングの箇所に対するテストファイルの作成

---
+++ DrintoTests/DrinkMemoryTests/DisplayDrinkMemoryTest.swift
@@ -0,0 +1,12 @@
+//
+//  DisplayDrinkMemory.swift
+//  Drinto
+//
+//  Created by HiroakiSaito on 2023/05/04.
+//
+
+import XCTest
+
+final class DisplayDrinkMemory: XCTestCase {
+    
+}
---
+++ DrintoTests/DrinkMemoryTests/EditDrinkMemoryTest.swift
@@ -0,0 +1,12 @@
+//
+//  EditDrinkMemoryTest.swift
+//  Drinto
+//
+//  Created by HiroakiSaito on 2023/05/04.
+//
+
+import XCTest
+
+final class EditDrinkMemoryTest: XCTestCase {
+
+}
